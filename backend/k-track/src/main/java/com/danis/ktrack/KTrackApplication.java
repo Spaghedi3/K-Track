b63afba89a29e2d9a347cdc55a59a38b57562a53
Daniszz <hritcudenis03@gmail.com>
2025-10-15 15:53:16 +0300

    back + ci action

---
+++ backend/k-track/src/main/java/com/danis/ktrack/KTrackApplication.java
@@ -0,0 +1,13 @@
+package com.danis.ktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KTrackApplication.class, args);
+    }
+
+}
---
+++ backend/k-track/src/test/java/com/danis/ktrack/KTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danis.ktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
